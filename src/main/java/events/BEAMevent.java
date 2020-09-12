b0d2f85e148e7e94b6465b2e9c365e5bbddc541d
aetizazsameer <67237406+aetizazsameer@users.noreply.github.com>
2020-09-12 03:49:24 +0000

    Update BEAMevent.java

--- src/main/java/events/BEAMevent.java
+++ src/main/java/events/BEAMevent.java
@@ -3,35 +3,70 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+
 public class BEAMevent extends ListenerAdapter {
    
    private boolean enabled = true;
-   private String prefix = "!";
+   private String prefix = "b!", messagestring;
    private String[] message;
+   
    
    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
       
-      message=event.getMessage().getContentRaw().split(" ");
+      messagestring=event.getMessage().getContentRaw();
+      message=messagestring.split(" ");
+      
       Member member = event.getMember();
       
       if(!member.getUser().isBot()) {
-         
-         if(enabled && message[0].toUpperCase().matches("☆?BEAM"))
-            send(event,"☆BEAM");
+         if(enabled) {
+            if(message[0].toUpperCase().matches("☆?BEAM"))
+               send(event,"☆BEAM");
+            
+            String[] greetings = new String[] {"Hi", "Hello", "Hey", "Howdy", "Greetings", "Sup dawg", "o/", "(´・◡・｀)/"};
+            for(String s: greetings)
+               if(messagestring.toUpperCase().contains(s.toUpperCase())) {
+                  send(event, s+"!");
+               }
+            
+            if(message.length>1 && (messagestring.toUpperCase().startsWith("I'M") ||
+               messagestring.toUpperCase().startsWith("IM")))
+                  send(event, "Hi, "+message[1]+"! I'm dad!");
+            if(message.length>2 && messagestring.startsWith("I AM"))
+               send(event, "Hi, "+message[2]+"! I'm dad!");
+         }
          
          if(message[0].startsWith(prefix)) {
-            if(!enabled && command("BEAMENABLE")) {
+            
+            if(!enabled && command("ENABLE")) {
                enabled=true;
                send(event, "☆BEAMbot enabled!");
             }
             
-            if(enabled && command("BEAMDISABLE")) {
-               enabled=false;
-               send(event, "☆BEAMbot disabled!");
+            if(enabled) {
+               if(command("DISABLE")) {
+                  enabled=false;
+                  send(event, "☆BEAMbot disabled!");
+               }
+               
+               if(command("BEAM"))
+                  send(event,"https://cdn.discordapp.com/attachments/443775272320499722/750354400161300572/BEAM.jpg");
+               
+               if(command("LINK"))
+                  send(event, "https://youtu.be/I25Cqlr5_Sc");
+               
+               if(command("INVITE"))
+                  send(event, "https://discord.com/api/oauth2/authorize?client_id=750688770780823582&permissions=29879360&scope=bot");
+               
+               if(message.length>1 && command("CHANGEPREFIX")) {
+                  prefix = message[1];
+                  send(event, "☆BEAMbot prefix changed to `"+prefix+"`!");
+               }
+               
+               if(command("PREFIX")) {
+                  send(event, "☆BEAMbot's current prefix is `"+prefix+"`!");
+               }
             }
-            
-            if(enabled && command("BEAM"))
-              send(event,"https://cdn.discordapp.com/attachments/443775272320499722/750354400161300572/BEAM.jpg");
          }
       }
    }
@@ -43,4 +78,5 @@ public class BEAMevent extends ListenerAdapter {
    private void send(GuildMessageReceivedEvent event, String message) {
       event.getChannel().sendMessage(message).queue();
    }
+  
 }
